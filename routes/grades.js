0bc947d98f31a05756f34761fd8451851ce4dfc5
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-03-26 19:44:40 -0300

    add post method to update a grade by id

--- routes/grades.js
+++ routes/grades.js
@@ -38,8 +38,36 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+router.put("/:id", async (req, res, next) => {
+  try {
+    const grades = JSON.parse(await fs.readFile(global.gradesFile));
+
+    const { student, subject, type, value } = req.body;
+
+    const studentIndex = grades.grades.findIndex(
+      (grade) => grade.id === Number(req.params.id)
+    );
+
+    const newGrade = {
+      id: Number(req.params.id),
+      student,
+      subject,
+      type,
+      value,
+    };
+
+    grades.grades[studentIndex] = newGrade;
+
+    await fs.writeFile(global.gradesFile, JSON.stringify(grades));
+
+    res.send(newGrade);
+  } catch (err) {
+    next(err);
+  }
+});
+
 /*
-- [ ]  Crie um endpoint para criar uma grade. Este endpoint deverá receber como parâmetros os campos student, subject, type e value,conforme descritos acima. Esta grade deverá ser salva no arquivo json grades.json, e deverá ter um id único associado. No campo timestamp,deverá ser salvo a data e hora do momento da inserção. O endpoint deverá retornar o objeto da grade que foi criada. A API deverá garantir oincremento automático deste identificador, de forma que ele não se repita entre os registros. Dentro do arquivo grades.json,que foi fornecido para utilização no desafio,o campo nextId já está com um valor definido. Após a inserção,é preciso que esse nextId seja incrementado e salvo no próprio arquivo, de forma que na próxima inserção ele possa ser utilizado.
+[] - Crie um endpoint para atualizar uma grade. Este endpoint deverá receber como parâmetros o id da grade a ser alterada e os campos student, subject, type e value. O endpoint deverá validar se a grade informada existee,caso não exista,deverá retornar um erro. Caso exista, o endpoint deverá atualizar as informações recebidas por parâmetros no registro e realizar sua atualização com os novos dados alterados no arquivo grades.json.
 */
 
 router.use((err, req, res, next) => {
